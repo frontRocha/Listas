23bd09bd3c923a1c616168ded897eec2afe0fb9f
estudantedehtml <mauriciopedeo007@gmail.com>
2022-10-31 14:21:25 -0300

    ajustes

--- list/src/App.tsx
+++ list/src/App.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState, useEffect } from 'react'
-import Card from './Components/Card/Card'
+import Card from './components/Card/Card'
 import './App.scss'
 
 export type Todo = {
